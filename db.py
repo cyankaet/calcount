9915d10d2b9c7bdbf2665030853476ca636afb23
Kate Meuse <katemeuse@gmail.com>
2021-06-26 16:50:04 -0400

    add names to meals, cals to food

--- db.py
+++ db.py
@@ -7,22 +7,34 @@ class Day(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
     meals = db.relationship("Meals", back_populates="day")
+
+    def __init__(self, **kwargs):
+        self.date = kwargs.get('date')
     
 class Meal(db.Model):
     __tablename__="meal"
     id = db.Column(db.Integer, primary_key=True)
     calories = db.Column(db.Integer, nullable=False)
-    time = db.Column(db.Time, nullable=False) #just use time?
+    name = db.Column(db.String, nullable=False)
+    time = db.Column(db.Time, nullable=False)
     day_id = db.Column(db.Integer, db.ForeignKey('day.id'))
     day = db.relationship("Day", back_populates="meals")
     foods = db.relationship("Foods", back_populates="meal")
 
     def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
         self.calories = kwargs.get('calories')
         self.time = kwargs.get('time')
 
 class Food(db.Model):
     __tablename__:"food"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    calories = db.Column(db.Integer, nullable=False)
     meal_id = db.Column(db.Integer, db.ForeignKey('meal.id'))
     meal = db.relationship("Meal", back_populates="foods")
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.calories = kwargs('calories')
+
