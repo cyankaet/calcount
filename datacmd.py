0ad7008b3cd22ffd2e2fb8fe18d91ba68f8e34ef
Kate Meuse <katemeuse@gmail.com>
2021-06-26 18:05:32 -0400

    remove day table

--- datacmd.py
+++ datacmd.py
@@ -1,12 +1,10 @@
 from db import Day, Meal, Food, db
 
-def new_meal(name, time, date):
-    day = Day.query.filter_by(date=date).first()
-    if day is None:
-        day = Day(date=date)
-        db.session.add(day)
-    meal = Meal(name=name, time=time)
+def new_meal(name, datetime):
+    meal = Meal(name=name, time=datetime)
     db.session.add(meal)
-    day.meals.append(meal)
     db.commit()
     return meal
+
+def new_food(meal_name, calories, date, name):
+    meal = Meal.query.filter_by(name)
--- db.py
+++ db.py
@@ -1,23 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-class Day(db.Model):
-    __tablename__="day"
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
-    meals = db.relationship("Meals", back_populates="day")
-
-    def __init__(self, **kwargs):
-        self.date = kwargs.get('date')
     
 class Meal(db.Model):
     __tablename__="meal"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    time = db.Column(db.Time, nullable=False)
-    day_id = db.Column(db.Integer, db.ForeignKey('day.id'))
-    day = db.relationship("Day", back_populates="meals")
+    time = db.Column(db.DateTime, nullable=False)
     foods = db.relationship("Foods", back_populates="meal")
 
     def __init__(self, **kwargs):
